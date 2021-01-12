f28ead760c1cdc6fc3f288282e70f876eba67f8e
ajay krishna <ajay.krishna@sysenso.com>
2021-01-12 18:03:20 +0530

    removed git commands

--- simModelTest.m
+++ simModelTest.m
@@ -1,18 +1,6 @@
 classdef simModelTest < matlab.unittest.TestCase
     methods(Test)
         function runModelAdvisor(testCase)
-            % fetch the branches.
-            [status,message] = dos('git fetch');
-            if status
-                testCase.verifyFail('Git fetch failture');
-                return;
-            end
-            % Checkout the branch name runTest.
-            [status,~] = dos('git checkout runTest');
-            if status
-                testCase.verifyFail('Git switch failture');
-                return;
-            end
             % load the model.
             load_system('sf_car');
             ma = Simulink.ModelAdvisor.getModelAdvisor('sf_car');
