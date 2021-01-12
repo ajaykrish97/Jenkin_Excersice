7e3cef87d5439dac9b316ae76aea8fe4e8d6995a
ajay krishna <ajay.krishna@sysenso.com>
2021-01-12 19:17:07 +0530

    added tearDown and classsetup

--- simModelTest.m
+++ simModelTest.m
@@ -1,11 +1,26 @@
 classdef simModelTest < matlab.unittest.TestCase
+    properties
+        model = 'sf_car';
+    end
+    
+    methods (TestClassSetup)
+        function loadModel(testCase)
+            load_system(testCase.model)
+        end
+    end
+    
+    methods (TestClassTeardown)
+        function closeModel(testCase)
+            bdclose(testCase.model)
+        end
+    end
+    
     methods(Test)
         function runModelAdvisor(testCase)
-            % load the model.
-            load_system('sf_car');
-            ma = Simulink.ModelAdvisor.getModelAdvisor('sf_car');
+            ma = Simulink.ModelAdvisor.getModelAdvisor(testCase.model);
             ma.selectCheckAll;
             ma.runCheck;
         end
+        
     end
 end
