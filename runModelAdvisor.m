0a290ca3db5a82317cf432e958ff472f5aad0730
ajay krishna <ajay.krishna@sysenso.com>
2021-01-12 16:54:17 +0530

    runtest branch

--- runModelAdvisor.m
+++
@@ -1,9 +0,0 @@
-function runModelAdvisor
-% Checkout the recently commited branch.
-
-% load the model.
-load_system('sim_autotrans');
-ma = Simulink.ModelAdvisor.getModelAdvisor('sim_autotrans');
-ma.selectCheckAll;
-ma.runCheck;
-end
