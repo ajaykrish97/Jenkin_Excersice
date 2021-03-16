5051d5123c18447a5ffa8f07132de3c3bdb12167
ajay krishna <ajay.krishna@sysenso.com>
2021-03-16 13:23:43 +0530

    write_csv_for_multiport.m added

--- multiport.m
+++ multiport.m
@@ -1,12 +1,16 @@
-function output = multiport(a,b,choice)
+function [input,output,msg] = multiport(a,b,choice)
 % multiport switch.
+msg = ['The choice selected is ' num2str(choice)];
+input = [a b];
 switch choice
     case 1
         output = a + b;
     case 2
         output = a-b;
-    case 3 
+    case 3
         output = a*b;
     otherwise
+        msg = 'The choice selected is invalid';
+        output = [];
 end
 end
